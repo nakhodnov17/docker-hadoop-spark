7f631053db1a99c58ba1fb8a0b56ad50a96ba140
Aleksandr <pocek2010@gmail.com>
2022-04-23 23:01:16 +0300

    Commit 2

--- examples/WebHDFS/python_client.py
+++
@@ -1,8 +0,0 @@
-import hdfs
-
-client = hdfs.Client('http://localhost:9870', root='/')
-print(client.list('/'))
-
-with client.write('/streaming.txt', encoding='utf-8') as hdfs_file:
-    for idx in range(10_000):
-        hdfs_file.write(f'{idx} {idx ** 2} {idx ** 3}\n')
--- examples/wordcount_streaming/run_python.sh
+++
@@ -1,1 +0,0 @@
-cat ${1} | python3 "src/mapper.py" | sort | python3 "src/reducer.py" > ${2}
